bfd06429888cd6371750cfbcfbe393c6c7f238f6
huadi73 <huadi73@hotmail.com>
2014-04-16 10:19:52 +0800

    1

---
+++ FileMD5/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace FileMD5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改這個方法的內容。
+        ///
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetHash = new System.Windows.Forms.Button();
+            this.txtMd5 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtSha1 = new System.Windows.Forms.TextBox();
+            this.btnVerify = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnGetHash
+            // 
+            this.btnGetHash.Location = new System.Drawing.Point(12, 12);
+            this.btnGetHash.Name = "btnGetHash";
+            this.btnGetHash.Size = new System.Drawing.Size(103, 39);
+            this.btnGetHash.TabIndex = 0;
+            this.btnGetHash.Text = "GetHash";
+            this.btnGetHash.UseVisualStyleBackColor = true;
+            this.btnGetHash.Click += new System.EventHandler(this.btnGetHash_Click);
+            // 
+            // txtMd5
+            // 
+            this.txtMd5.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtMd5.Location = new System.Drawing.Point(12, 163);
+            this.txtMd5.Multiline = true;
+            this.txtMd5.Name = "txtMd5";
+            this.txtMd5.ReadOnly = true;
+            this.txtMd5.Size = new System.Drawing.Size(482, 29);
+            this.txtMd5.TabIndex = 1;
+            this.txtMd5.TextChanged += new System.EventHandler(this.txtMd5_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txtSha1
+            // 
+            this.txtSha1.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtSha1.Location = new System.Drawing.Point(12, 88);
+            this.txtSha1.Multiline = true;
+            this.txtSha1.Name = "txtSha1";
+            this.txtSha1.ReadOnly = true;
+            this.txtSha1.Size = new System.Drawing.Size(482, 29);
+            this.txtSha1.TabIndex = 3;
+            // 
+            // btnVerify
+            // 
+            this.btnVerify.Location = new System.Drawing.Point(134, 12);
+            this.btnVerify.Name = "btnVerify";
+            this.btnVerify.Size = new System.Drawing.Size(100, 39);
+            this.btnVerify.TabIndex = 4;
+            this.btnVerify.Text = "vMD5";
+            this.btnVerify.UseVisualStyleBackColor = true;
+            this.btnVerify.Click += new System.EventHandler(this.btnVerify_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("新細明體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtResult.Location = new System.Drawing.Point(12, 237);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResult.Size = new System.Drawing.Size(482, 188);
+            this.txtResult.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(9, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "SHA1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(12, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "MD5";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(12, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Result";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 442);
+            this.progressBar1.MarqueeAnimationSpeed = 1;
+            this.progressBar1.Maximum = 0;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(482, 31);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 485);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnVerify);
+            this.Controls.Add(this.txtSha1);
+            this.Controls.Add(this.txtMd5);
+            this.Controls.Add(this.btnGetHash);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FileMD5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetHash;
+        private System.Windows.Forms.TextBox txtMd5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtSha1;
+        private System.Windows.Forms.Button btnVerify;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ FileMD5/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Threading;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Security.AccessControl;
+
+
+namespace FileMD5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            backgroundWorker1.WorkerReportsProgress = true;//允許BackgroundWorker報告進度
+
+            if (txtResult.Text == "")
+                btnVerify.Enabled = false;
+        }
+        public String HashString(byte[] data)
+        {
+            string hash = "";
+            for (int i = 0; i < data.Length; i++)
+                hash += data[i].ToString("X");
+            return hash;
+        }
+        long fileSize = 0;//選擇的檔案大小
+        private void btnGetHash_Click(object sender, EventArgs e)
+        {
+            fileSize = 0;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();//引用stopwatch物件
+                sw.Reset();//碼表歸零
+                sw.Start();//碼表開始計時
+                byte[] md5data = new byte[32];
+                byte[] shadata = new byte[64];
+                Thread thread1 = new Thread(new ThreadStart(() =>
+                    {
+                        using (FileStream fs = File.OpenRead(openFileDialog1.FileName))
+                        {
+                            MD5 md5 = new MD5CryptoServiceProvider();
+                            md5data = md5.ComputeHash(fs);
+
+                            fileSize = fs.Length;
+                            fs.Close();
+                        }
+                    }));
+                Thread thread2 = new Thread(new ThreadStart(() =>
+                   {
+                       using (FileStream fs = File.OpenRead(openFileDialog1.FileName))
+                       {
+                           SHA256Managed sha = new SHA256Managed();
+                           shadata = sha.ComputeHash(fs);
+
+                           fileSize = fs.Length;
+                           fs.Close();
+                       }
+                   }));
+                thread1.Start(); thread2.Start();
+                thread1.Join(); thread2.Join();
+                if (thread1.ThreadState == ThreadState.Stopped && thread2.ThreadState == ThreadState.Stopped)
+                {
+                    sw.Stop();//碼錶停止
+                    string result1 = sw.Elapsed.TotalSeconds.ToString();//印出所花費的總豪秒數
+                    txtSha1.Text = HashString(shadata);
+                    txtMd5.Text = HashString(md5data);
+                    txtResult.Text = " 時間 :" + result1;
+                }
+            }
+        }
+
+        int barMaxValue = 0;//進度條的最大值(檔案個數)
+        string matchText = "";//符合的檔案
+        private void btnVerify_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtResult.Text = "";
+                barMaxValue = 0;
+                matchText = "";
+                                
+                DirectoryInfo dirInfo = new DirectoryInfo(folderBrowserDialog1.SelectedPath);
+                #region 得到資料夾的讀取權限
+                DirectorySecurity ds = dirInfo.GetAccessControl();
+                FileSystemAccessRule ar1 = new FileSystemAccessRule("Administrators", FileSystemRights.Read, AccessControlType.Allow);
+                FileSystemAccessRule ar2 = new FileSystemAccessRule("Administrators", FileSystemRights.Read, InheritanceFlags.ContainerInherit | InheritanceFlags.ObjectInherit, PropagationFlags.InheritOnly, AccessControlType.Allow);
+
+                ds.AddAccessRule(ar1);
+                ds.AddAccessRule(ar2);
+                dirInfo.SetAccessControl(ds);
+                #endregion //得到資料夾的讀取權限
+
+                //barMaxValue = dirInfo.GetFiles("*.*", SearchOption.AllDirectories).Length;
+                foreach (FileInfo file in dirInfo.GetFiles("*.*", SearchOption.AllDirectories))
+                {
+                    try//取得(子)目錄下所有檔案個數
+                    {
+                        if (dirInfo.Attributes != FileAttributes.Hidden && dirInfo.Attributes != FileAttributes.System)
+                            barMaxValue++;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex.Message);
+                    }
+                }
+                progressBar1.Maximum = barMaxValue;
+
+                backgroundWorker1.RunWorkerAsync(barMaxValue);//使用RunWorkerAsync方法，觸動DoWork事件
+            }
+        }
+
+        private void Worker(BackgroundWorker bkWorker)
+        {
+            int barCurrent = 0;//進度條目前的值
+
+            DirectoryInfo dirInfo = new DirectoryInfo(folderBrowserDialog1.SelectedPath);
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;//比較字串
+            String verityBase = txtMd5.Text;//以此字串為基底來比較其他
+
+            //foreach (FileInfo file in dirInfo.GetFiles("*.*", SearchOption.AllDirectories))
+            Parallel.ForEach(dirInfo.EnumerateFiles("*.*", SearchOption.AllDirectories), file =>
+            {
+                try
+                {
+                    if (dirInfo.Attributes != FileAttributes.Hidden && dirInfo.Attributes != FileAttributes.System)
+                        using (FileStream fs = File.OpenRead(file.FullName.ToString()))
+                        {
+                            if (fs.Length == fileSize)//如果找到的檔案與基底檔案的大笑不同，就可以跳過了
+                            {
+                                MD5 md5 = new MD5CryptoServiceProvider();
+                                byte[] md5data = md5.ComputeHash(fs);
+                                fs.Close();
+
+                                if (comparer.Compare(verityBase, HashString(md5data)) == 0)//比對結果相符，則顯示檔案位置
+                                    matchText += file.FullName.ToString() + "\r\n";
+                            }
+                            barCurrent++;//進度條目前的值
+                            bkWorker.ReportProgress(barCurrent, barCurrent.ToString());//執行ReportProgress方法，觸發ProgressChanged事件
+                        }
+                    //Thread.Sleep(1);//防止更新太快無法顯示
+                    Application.DoEvents();
+                }
+                catch (Exception ex)
+                {
+                    //系統的"另一個程序正在使用檔案"
+                    //MessageBox.Show("Error: " + ex.Message);
+                }
+            });
+        }
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            //允許BackgroundWorker報告進度
+            backgroundWorker1.WorkerReportsProgress = true;
+
+            progressBar1.Maximum = barMaxValue;
+
+            //在方法中傳遞BackgroundWorker參數
+            Worker(sender as BackgroundWorker);
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {//回傳執行結果
+            try
+            {
+                txtResult.Text = "正在處理" + e.UserState.ToString() + " / " + progressBar1.Maximum.ToString();
+                txtResult.Update();
+
+                progressBar1.Maximum = barMaxValue;
+                progressBar1.Value = e.ProgressPercentage;
+
+                //Thread.Sleep(1);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (matchText == "")
+                txtResult.Text = "共 " + barMaxValue.ToString() + " 個檔案\r\n" + "沒有符合的";
+            else
+                txtResult.Text = "共 " + barMaxValue.ToString() + " 個檔案\r\n" + "符合的有 :\r\n" + matchText;
+        }
+
+        private void txtMd5_TextChanged(object sender, EventArgs e)
+        {
+            btnVerify.Enabled = true;
+        }
+
+
+
+    }
+}
